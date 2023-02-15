f8aeb92dd0d48da4cba9b577b486d489cfd68f56
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2023-02-15 10:55:07 -0300

    First command operational

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+
+// import .env
 const dotenv = require("dotenv");
-
-// Load environment variables from .env file
 dotenv.config();
-
-// Get the token from the environment variables
-const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
+const { TOKEN } = process.env;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+// import commands
+const fs = require("node:fs");
+const path = require("node:path");
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const commandPath = path.join(commandsPath, file);
+  const command = require(commandPath);
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  }
+}
+
+// Log in to Discord with your client's token
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-
-// Log in to Discord with your client's token
 client.login(TOKEN);
+
+// Listener for slash commands
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
